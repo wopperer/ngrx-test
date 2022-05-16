a51a9fdec4d7eab88b64b917528eb9d8e05f9df0
MW <marcel.wopperer@gmail.com>
2022-05-16 18:52:54 +0200

    0398475023897459888

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'; // Angular CLI environment
+ 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyCounterComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({ count: counterReducer })],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
